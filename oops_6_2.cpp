4fae82642f1bfe86d7d7c06dc317ea6e52e6f922
mvishalsingh25 <161185653+mvishalsingh25@users.noreply.github.com>
2024-10-08 12:08:35 +0530

    Update oops_6_2.cpp

--- oops_6_2.cpp
+++ oops_6_2.cpp
@@ -4,17 +4,17 @@ using namespace std;
 
 class Employee{
     string name;
-    int phoneNo;
+    long long phoneNo;
 
 public:
    Employee(){
     cout<<"Default constructor is called----> Employee"<<endl;
    }
-   Employee(string n, int ph):name(n),phoneNo(ph){
+   Employee(string n, long long ph):name(n),phoneNo(ph){
     cout<<"Parameterised constructor is called---->Employee"<<endl;
    }
 
-   void putData(string name, int PhoneNo){
+   void putData(string name, long long PhoneNo){
    this->name=name;
    this->phoneNo=PhoneNo;
    }
@@ -28,25 +28,28 @@ public:
     cout<<"Destructor is called --->Employee"<<endl;
  }
 };
+
 class Manager : public Employee{
     string title;
-     int club_dues;
+     float club_dues;
 
 public:
 
    Manager(){
+   Employee();
     cout<<"Default constructor is called--->Manager"<<endl;
    }
   
-  Manager (string n, int pn , string t, int cd):Employee(n,pn),title(t),club_dues(cd){};
+  Manager (string n, long long pn , string t, int cd):Employee(n,pn),title(t),club_dues(cd){};
 
-  void putData(string n ,  int pn ,string t, int cd){
+  void putData(string n ,  long long  pn ,string t, float cd){
      Employee::putData(n,pn);
      title = t;
      club_dues=cd;
   }
 
   void getData(){
+     cout<<"Manager: "<<endl;
      Employee::getData();
      cout<<"Title: "<<title<<endl;
      cout<<"Club Dues : "<<club_dues<<endl;
@@ -56,35 +59,73 @@ public:
     cout<<"Destructor is called --> Manager"<<endl;
   }
 };
+
 class Scientist: public Employee{
-    string publications;
+    int publications;
 
     public:
     Scientist(){
+    Employee();
         cout<<"Default constructor is called --.Scientist"<<endl;
     }
-    Scientist( string n, int pn,string publications){
-        Employee(n,pn);
-       this->publications=publications;
-    }
+    Scientist( string n, long long  pn,int pb):Employee(n,pn),publications(pb){};
 
-    void putData(){
-  
+    void putData( string n, long long pn,int publications){
+       Employee::putData(n,pn);
+       this->publications=publications;
     }
     
     void getData(){
-
+      cout<<"Scientist: "<<endl;
+      Employee::getData();
+      cout<<"Publications: "<<publications<<endl;
     }
+    
+    ~Scientist(){
+        cout<<"Destructor is called --> Scientist"<<endl;
+    }
+    
 };
 class Laborer:public Employee{
+public:
+  Laborer(){
+Employee();
+ cout<<"Default constructor is called -->Laborer"<<endl;
+}
+
+Laborer(string n,long long pn):Employee(n,pn){};
+
+void putData(string n,long long pn){
+ Employee::putData(n,pn);
+}
+
+void getData(){
+Employee::getData();
+}
+
+~Laborer(){
+          cout<<"Destructor is called --> Laborer"<<endl;
+}
 
 };
+
 int main(){
 cout<<endl;
 Employee e1("Rohan Gupta",9983725374);
 e1.getData();
-Manager m1("Tarun",9906353251,"Project Manager",12435);
+cout<<endl;
+
+Manager m1("Tarun Khajuria",9906353251,"Project Manager",5000.00);
 m1.getData();
+cout<<endl;
+
+Scientist s1("Hardik Singh",9908765456,6);
+s1.getData();
+cout<<endl;
+
+Laborer l1("Sonu",9987546231);
+l1.getData();
+
 cout<<endl;
 
 return 0;
